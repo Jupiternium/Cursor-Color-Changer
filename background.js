feab9faee5a543adf5dfb880341800b5e0517ec1
Johnathan Davis <110743855+Jupiternium@users.noreply.github.com>
2023-07-15 21:48:34 -0700

    fixed unresponsive buttons in background.js

--- background.js
+++ background.js
@@ -1,21 +1,20 @@
 // Add a message event listener to the background script
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    // Check the action of the message
-    switch (message.action) {
-      // If the action is "new-tab"
-      case "new-tab":
-        // Create a new tab with your extension URL
-        chrome.tabs.create({url: chrome.extension.getURL("index.html")});
-        break;
-      // If the action is "new-window"
-      case "new-window":
-        // Create a new window with your extension URL
-        chrome.windows.create({url: chrome.extension.getURL("index.html")});
-        break;
-      // If the action is something else
-      default:
-        // Do nothing
-        break;
-    }
-  });
-  
+  // Check the action of the message
+  switch (message.action) {
+    // If the action is "new-tab"
+    case "new-tab":
+      // Create a new tab with your extension URL
+      chrome.windows.open('https://google.com');
+      break;
+    // If the action is "new-window"
+    case "new-window":
+      // Create a new window with your extension URL
+      chrome.windows.open('https://google.com');
+      break;
+    // If the action is something else
+    default:
+      // Do nothing
+      break;
+  }
+});
--- popup.js
+++ popup.js
@@ -4,12 +4,12 @@ let newWindowButton = document.getElementById("new-window");
 
 // Add a click event listener to the new tab button
 newTabButton.addEventListener("click", function() {
-  // Send a message to the background script with the action "new-tab"
-  chrome.runtime.sendMessage({action: "new-tab"});
+  window.open("https://google.com");
 });
+
+
 
 // Add a click event listener to the new window button
 newWindowButton.addEventListener("click", function() {
-  // Send a message to the background script with the action "new-window"
-  chrome.runtime.sendMessage({action: "new-window"});
+  window.open("https://google.com", "New Window");
 });
